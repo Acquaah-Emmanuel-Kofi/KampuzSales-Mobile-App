fa59ee6e77cdcc81807b3c63c1a1be765820f5b9
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-03 21:30:36 +0000

    Design Login Screen

--- src/FrontendDirectory/screens/LoginScreen.js
+++ src/FrontendDirectory/screens/LoginScreen.js
@@ -1,12 +1,92 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { StyleSheet ,Text, TextInput, View } from "react-native";
+import CustomButton from "../components/CustomButton";
+import GoogleCustomButton from "../components/GoogleCustomButton";
+import  Colors  from "../data/colors";
 
 function LoginScreen() {
     return (
-        <View>
-            <Text>LoginScreen</Text>
+        <View style={styles.container}>
+            <Text style={styles.heading}>Welcome Back , Login</Text>
+            <View style={styles.inputBox}>
+                <Text style={styles.label}>Email</Text>
+                <TextInput 
+                    style={styles.textInputBox} 
+                    placeholderTextColor={Colors.secondary}
+                    placeholder="robert.sam@gmail.com" 
+                />
+            </View>
+            <View style={styles.inputBox}>
+                <Text style={styles.label}>Password</Text>
+                <TextInput 
+                    style={styles.textInputBox} 
+                    secureTextEntry={true}
+                    placeholderTextColor={Colors.secondary}
+                    placeholder="*************" 
+                />
+            </View>
+            <View style={styles.inputBox}>
+                <Text style={styles.forgotPasswordLinkText}>Forgot password</Text>
+            </View>
+            <CustomButton buttonText={"Sign In"} />
+            <GoogleCustomButton buttonText={"Sign in with Google"} />
+            <Text style={styles.switchLoginScreenText}>
+                Don’t have an account? <Text style={styles.switchLoginScreenLinkText}>Sign up</Text>
+            </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: Colors.white,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    heading: {
+        fontSize: 30,
+        fontStyle: 'normal',
+        fontWeight: '600',
+        color: Colors.black,
+        marginBottom: 20,
+        letterSpacing: 2,
+    },
+    inputBox: {
+        width: '85%',
+        marginVertical: 10,
+    },
+    label: {
+        fontSize: 14,
+        fonfontStyle: 'normal',
+        color: Colors.labelGray,
+        fontWeight: '500',
+        marginBottom: 6,
+    },
+    textInputBox: {
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: Colors.borderGray,
+        paddingHorizontal: 10,
+        paddingVertical: 14,
+        color: Colors.secondary,
+        gap: 8,
+    },
+    switchLoginScreenText: {
+        fontSize: 14,
+        fontWeight: '400',
+        color: Colors.secondary,
+        marginVertical: 12,
+    },
+    switchLoginScreenLinkText: {
+        color: Colors.main,
+    },
+    forgotPasswordLinkText: {
+        color: Colors.main,
+        fontSize: 14,
+        fontWeight: '500',
+        textAlign: 'right',
+    }
+  });
 
 export default LoginScreen;
