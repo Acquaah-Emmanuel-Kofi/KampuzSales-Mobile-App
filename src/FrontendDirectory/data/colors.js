f2d1eb6e2973392a3924b1d7698ff19986e99e6f
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-03 20:45:49 +0000

    Update colors

--- src/FrontendDirectory/data/colors.js
+++ src/FrontendDirectory/data/colors.js
@@ -3,7 +3,9 @@ const Colors = {
     white: "#F5F5F5",
     black: "#2F2E41",
     subBlack: "#909090",
-    secondary: "667085",
+    secondary: "#667085",
+    labelGray: "#344054",
+    borderGray: "#D0D5DD",
 };
 
 export default Colors;
