5c431c48579344f32f518e4c159c5fdf19b22abf
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-03 20:30:05 +0000

    Add custom buttom for sign up and sign in

---
+++ src/FrontendDirectory/components/CustomButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import  Colors  from "../data/colors";
+
+function CustomButton({buttonText}) {
+    return (
+        <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>{buttonText}</Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: Colors.main,
+        backgroundColor: Colors.main,
+        paddingHorizontal: 10,
+        paddingVertical: 14,
+        gap: 8,
+        marginVertical: 12,
+        width: '85%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: Colors.white,
+        fontWeight: '500',
+        fontSize: 16,
+    }
+  });
+
+export default CustomButton;
