0f9b4e35ddce3caa5a77b1f41d853118b56ef0d7
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-05 01:16:24 +0000

    Add HomeProduct component to HomeScreen

--- src/FrontendDirectory/screens/HomeScreen.js
+++ src/FrontendDirectory/screens/HomeScreen.js
@@ -1,12 +1,23 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import HomeCategories from "../components/HomeCategories";
+import HomeProducts from "../components/HomeProducts";
+import  Colors  from "../data/colors";
 
 function HomeScreen() {
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View style={styles.container}>
+            <HomeCategories />
+            <HomeProducts />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: Colors.white,
+    },
+  });
 
 export default HomeScreen;
