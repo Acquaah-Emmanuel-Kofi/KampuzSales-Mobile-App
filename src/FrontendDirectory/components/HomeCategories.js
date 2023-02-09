33dd5e37256c500be784418e414762c23753fb4f
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-09 18:16:09 +0000

    Update hoem categories component

--- src/FrontendDirectory/components/HomeCategories.js
+++ src/FrontendDirectory/components/HomeCategories.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View, Image } from "react-native";
 import  Colors  from "../data/colors";
+import categoriesData from "../data/categoriesData";
 
 function HomeCategories() {
+
+  const renderCategoryItem = ({ item }) => {
+    return (
+      <View>
+        <Pressable style={styles.navCategoryItems}>
+          <View style={styles.navCategoryIcon}>
+            <Image source={item.image} />
+          </View>
+          <Text style={styles.navCategoryName}>{item.name}</Text>
+        </Pressable>
+      </View>
+    )
+  }
     return (
         <View style={styles.container}>
-            <Text>HomeCategories</Text>
+            <View>
+              <FlatList 
+                data={categoriesData}
+                renderItem={renderCategoryItem}
+                keyExtractor={(item) => item._id}
+                horizontal={true}
+              />
+            </View>
         </View>
     )
 }
@@ -17,6 +38,27 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    navCategoryItems: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 20,
+      paddingHorizontal: 15,
+    },
+    navCategoryIcon: {
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: Colors.favIconBg,
+        backgroundColor: Colors.favIconBg,
+        width: 50,
+        height: 50,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    navCategoryName: {
+      paddingTop: 10,
+    }
   });
 
 export default HomeCategories;
