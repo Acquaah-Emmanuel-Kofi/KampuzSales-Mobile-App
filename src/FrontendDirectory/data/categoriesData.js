6f0cb80dc9a2fa8b36aed9550ffe44a47b83d788
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-09 18:15:24 +0000

    Add demo categoriesData component

---
+++ src/FrontendDirectory/data/categoriesData.js
@@ -0,0 +1,29 @@
+const categoriesData = [
+    {
+        _id: "1",
+        name: "Popular",
+        image: require('./images/CategoriesIcon/star.png'),
+    },
+    {
+        _id: "2",
+        name: "Chair",
+        image: require('./images/CategoriesIcon/chair.png'),
+    },
+    {
+        _id: "3",
+        name: "Table",
+        image: require('./images/CategoriesIcon/table.png'),
+    },
+    {
+        _id: "4",
+        name: "Armchair",
+        image: require('./images/CategoriesIcon/armchair.png'),
+    },
+    {
+        _id: "5",
+        name: "Bed",
+        image: require('./images/CategoriesIcon/bed.png'),
+    },
+]
+
+export default categoriesData;
