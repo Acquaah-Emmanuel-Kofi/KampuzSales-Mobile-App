8925f0f25256525ba1daf0380796890c36acc7e8
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-09 18:17:18 +0000

    Update button Navigation images with Icons

--- src/FrontendDirectory/navigations/ButtomNavigation.js
+++ src/FrontendDirectory/navigations/ButtomNavigation.js
@@ -1,12 +1,16 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Image, Text, View } from 'react-native';
 import Colors from '../data/colors';
-import HomeScreen from '../screens/HomeScreen';
 import PostScreen from '../screens/PostScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import SearchScreen from '../screens/SearchScreen';
 import WishlistScreen from '../screens/WishlistScreen';
 import StackNavigation from './StackNavigation';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Feather from "react-native-vector-icons/Feather";
+import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const Tab = createBottomTabNavigator();
 
@@ -24,49 +28,40 @@ const ButtomNavigation = () => {
             <Tab.Screen name='Main' component={StackNavigation} options={{
                 tabBarIcon: ({focused}) => (
                     <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                        <Image
-                            source={require("../data/images/NavigationIcons/HomeIcon.png")}
-                            resizeMode='contain'
-                            style={{
-                                tintColor: focused ? Colors.main : Colors.subBlack
-                            }}
-                        />
-                        {/* <Text style={{color: focused ? Colors.main : Colors.subBlack}}>Home</Text> */}
+                        <View>
+                            {focused ? (
+                                <Ionicons name="ios-home" size={24} color={Colors.main} />
+                                ) : ( 
+                                <Ionicons name="ios-home-outline" size={24} color={Colors.subBlack} />
+                            )}
+                        </View>
+                        <Text style={{color: focused ? Colors.main : Colors.subBlack, fontSize: 12}}>Home</Text>
                     </View>
                 ),
             }}></Tab.Screen>
             <Tab.Screen name='WhishList' component={WishlistScreen} options={{
                 tabBarIcon: ({focused}) => (
                     <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                        <Image
-                            source={require("../data/images/NavigationIcons/WhishList.png")}
-                            resizeMode='contain'
-                            style={{
-                                tintColor: focused ? Colors.main : Colors.subBlack
-                            }}
-                        />
-                        {/* <Text style={{color: focused ? Colors.main : Colors.subBlack}}>WhishList</Text> */}
+                        <View>
+                            {focused ? (
+                                <MaterialIcons name="favorite" size={24} color={Colors.main} />
+                                ) : ( 
+                                <MaterialIcons name="favorite-outline" size={24} color={Colors.subBlack} />
+                            )}
+                        </View>
+                        <Text style={{color: focused ? Colors.main : Colors.subBlack, fontSize: 12}}>WhishList</Text>
                     </View>
                 ),
             }}></Tab.Screen>
             <Tab.Screen name='Post' component={PostScreen} options={{
                 tabBarIcon: ({focused}) => (
                     <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                        <View style={{
-                            backgroundColor: Colors.main,
-                            borderRadius: '50%',
-                            width: 50,
-                            height: 50,
-                            marginTop: -20,
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}>
-                            <Text style={{
-                                fontSize: 20,
-                                fontWeight: '600',
-                                color: Colors.white
-                            }}>+</Text>
+                        <View>
+                            {focused ? (
+                                <AntDesign name="pluscircle" size={50} color={Colors.main} style={{marginTop: -40}} />
+                                ) : ( 
+                                <AntDesign name="pluscircle" size={50} color={Colors.subBlack} style={{marginTop: -40}} />
+                            )}
                         </View>
                         {/* <Text style={{color: focused ? Colors.main : Colors.subBlack}}>Post</Text> */}
                     </View>
@@ -75,28 +70,28 @@ const ButtomNavigation = () => {
             <Tab.Screen name='Search' component={SearchScreen} options={{
                 tabBarIcon: ({focused}) => (
                     <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                        <Image
-                            source={require("../data/images/NavigationIcons/Search.png")}
-                            resizeMode='contain'
-                            style={{
-                                tintColor: focused ? Colors.main : Colors.subBlack
-                            }}
-                        />
-                        {/* <Text style={{color: focused ? Colors.main : Colors.subBlack}}>Search</Text> */}
+                        <View>
+                            {focused ? (
+                                <Feather name="search" size={24} color={Colors.main} />
+                                ) : ( 
+                                <Feather name="search" size={24} color={Colors.subBlack} />
+                            )}
+                        </View>
+                        <Text style={{color: focused ? Colors.main : Colors.subBlack, fontSize: 12}}>Search</Text>
                     </View>
                 ),
             }}></Tab.Screen>
             <Tab.Screen name='Profile' component={ProfileScreen} options={{
                 tabBarIcon: ({focused}) => (
                     <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                        <Image
-                            source={require("../data/images/NavigationIcons/Profile.png")}
-                            resizeMode='contain'
-                            style={{
-                                tintColor: focused ? Colors.main : Colors.subBlack
-                            }}
-                        />
-                        {/* <Text style={{color: focused ? Colors.main : Colors.subBlack}}>Profile</Text> */}
+                        <View>
+                            {focused ? (
+                                <Icon name="ios-person" size={24} color={Colors.main} />
+                                ) : ( 
+                                <Icon name="ios-person-outline" size={24} color={Colors.subBlack} />
+                            )}
+                        </View>
+                        <Text style={{color: focused ? Colors.main : Colors.subBlack, fontSize: 12}}>Profile</Text>
                     </View>
                 ),
                 tabBarBadge: 3
