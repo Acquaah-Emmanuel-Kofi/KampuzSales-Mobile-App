cd8c9dbc29441826893699cdd36889a539264933
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-11 01:26:10 +0000

    Remove unwanted line of code

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
 import Tabs from './src/FrontendDirectory/navigations/tabs';
-import LoginScreen from './src/FrontendDirectory/screens/LoginScreen'
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <StatusBar hidden={true} /> */}
       <Tabs />
     </NavigationContainer>
   );
