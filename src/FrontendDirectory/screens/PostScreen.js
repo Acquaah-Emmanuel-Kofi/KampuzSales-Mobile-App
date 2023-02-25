d0322028ed8e7fd0c03367625f6d2f731e604bc9
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-25 03:47:55 +0000

    Update PostScreen

--- src/FrontendDirectory/screens/PostScreen.js
+++ src/FrontendDirectory/screens/PostScreen.js
@@ -1,19 +1,62 @@
-import React from "react";
-import { KeyboardAvoidingView, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, Image, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import HeadTitle from "../components/HeadTitle";
 import  Colors  from "../data/colors";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
+import * as ImagePicker from 'expo-image-picker';
+import { firebase } from '../../BackendDirectory/config';
+
 function PostScreen() {
+
+     const [ image, setImage ] = useState(null);
+     const [ uploading, setUploading ] = useState(false);
+
+
+     const pickImage = async () => {
+        // no permission is rewuired
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            // allowsEditing: true,
+            aspect: [4,3],
+            allowsMultipleSelection: true,
+            quality: 1
+        });
+
+        if (!result.canceled) {
+            const source = {uri: result.assets[0].uri};
+            setImage(source);
+        }
+
+     }
+
+     const uploadImage = async () => {
+        setUploading(true);
+        const response = await fetch(image.uri);
+        const blob = await response.blob();
+        const filename = image.uri.substring(image.uri.lastIndexOf('/') + 1);
+        var ref = firebase.storage().ref().child(filename).put(blob);
+
+        try {
+            await ref;
+        } catch(e){
+            console.log(e);
+        }
+
+        setUploading(false);
+
+        setImage(null);
+     }
+     
     return (
         <View style={styles.container}>
             <ScrollView contentContainerStyle={styles.innerContainer}>
                 <HeadTitle title={"Sell"} />
                 <View style={styles.postAndThumbnail}>
                     <View style={styles.thumbnail}>
-                        <Text>Thumbnail</Text>
+                        { image && <Image source={{uri: image.uri}} resizeMode='stretch' style={styles.image} />}
                     </View>
-                    <Pressable style={styles.postIconView} onPress={() => alert("Upload!")}>
+                    <Pressable style={styles.postIconView} onPress={pickImage}>
                         <MaterialCommunityIcons style={styles.postIcon} name="plus-circle-multiple" size={50} color={Colors.main} />
                     </Pressable>
                 </View>
@@ -43,7 +86,7 @@ function PostScreen() {
                     <Pressable style={styles.clearButtom} onPress={() => alert("Cleared!")}>
                         <Text style={styles.clearButtomText}>Clear</Text>
                     </Pressable>
-                    <Pressable style={styles.postButtom} onPress={() => alert("Posted!")}>
+                    <Pressable style={styles.postButtom} onPress={uploadImage}>
                         <Text style={styles.postButtomText}>Post</Text>
                     </Pressable>
                 </View>
@@ -72,6 +115,10 @@ const styles = StyleSheet.create({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    image: {
+        width: 140,
+        height: 140,
     },
     postIconView: {
         width: '35%',
