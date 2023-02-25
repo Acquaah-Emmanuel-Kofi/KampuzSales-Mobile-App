764102cbb553b48182e3bedd28ddc29241f332c0
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-25 03:50:57 +0000

    Update App component with auth

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import Tabs from './src/FrontendDirectory/navigations/tabs';
+import { auth }  from './src/BackendDirectory/config';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ButtomNavigation from './src/FrontendDirectory/navigations/BottomNavigation';
 
-export default function App() {
-  return (
-    <NavigationContainer>
+
+const AppStack = createNativeStackNavigator();
+
+function App() {
+
+  const [ initializing, setInitializing ] = useState(true);
+  const [ user, setUser ] = useState();
+
+  function onAuthStateChanged(user){
+    setUser(user);
+    if(initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth.onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, [])
+
+  if(initializing) return null;
+
+  if(!user){
+    return (
       <Tabs />
-    </NavigationContainer>
+    );
+  }
+
+
+  return (
+        <AppStack.Navigator
+            screenOptions={{
+                tabBarShowLabel: false,
+                headerShown: false,
+                tabBarStyle: { height: 0 },
+              }}
+          >
+            <AppStack.Screen name='Buttom' component={ButtomNavigation}></AppStack.Screen>
+        </AppStack.Navigator>
   );
 }
 
+export default () => {
+    return (
+      <NavigationContainer>
+        <App />
+      </NavigationContainer>
+  );
+}
