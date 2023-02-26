6d52a15690fe9b238a7b192a0bf4a20cc5790a10
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-02-26 05:04:28 +0000

    Update firebase config file

--- src/BackendDirectory/config.js
+++ src/BackendDirectory/config.js
@@ -1,16 +1,3 @@
-// import * as firebase from 'firebase';
-// import 'firebase/auth';
-// import 'firebase/firestore';
-// import 'firebase/storage';
-
-// firebase.initializeApp(firebaseConfig);
-
-// const auth = firebase.auth();
-// const db = firebase.firestore();
-// const storage = firebase.storage();
-
-// export { auth, db, storage };
-
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
@@ -26,17 +13,13 @@ const firebaseConfig = {
   measurementId: "G-7BVM730DGF"
 };
 
-let app;
 
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig)
-} else {
-  app = firebase.app();
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
 }
 
 const auth = firebase.auth();
-const database = app.firestore();
 const storage = firebase.storage();
 const firestore = firebase.firestore();
 
-export { database, auth, storage, firestore };
+export { auth, storage, firestore, firebase };
