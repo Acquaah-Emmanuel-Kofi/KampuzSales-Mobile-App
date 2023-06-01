7535cd77718ec00709f38bf035106abfd746a971
waglarry <emmanuelacquaah2468@gmail.com>
2023-06-01 09:16:52 +0000

    add a function to refresh homeScreen

--- src/FrontendDirectory/screens/HomeScreen.js
+++ src/FrontendDirectory/screens/HomeScreen.js
@@ -1,16 +1,34 @@
-import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, View, RefreshControl } from "react-native";
 import HomeCategories from "../components/HomeScreenComponents/HomeCategories";
 import HomeNavbar from "../components/HomeScreenComponents/MainNavbar";
 import HomeProducts from "../components/HomeScreenComponents/HomeProducts";
 import  Colors  from "../data/colors";
 
 function HomeScreen() {
+
+  const [ refresh, setRefresh ] = useState(false);
+
+  const pullToRefresh = () => {
+    setRefresh(true);
+
+    setTimeout(() => {
+      setRefresh(false);
+    }, 2000)
+  }
+
     return (
-        <View style={styles.container}>
-            <ScrollView contentInsetAdjustmentBehavior="automatic">
-              <HomeNavbar />
-              <HomeCategories />
+        <View style={styles.container}>            
+        <HomeNavbar />
+            <ScrollView 
+              contentInsetAdjustmentBehavior="automatic"
+              refreshControl={
+                <RefreshControl 
+                  refreshing={refresh}
+                  onRefresh={() => pullToRefresh()}
+                />
+              }
+              >
               <HomeProducts />
             </ScrollView>
         </View>
