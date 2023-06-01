95855684f4a625f09d8655f6f3127aa7c14b99f2
waglarry <emmanuelacquaah2468@gmail.com>
2023-06-01 09:14:38 +0000

    fetch data prices for cart total price

--- src/FrontendDirectory/screens/CartScreen.js
+++ src/FrontendDirectory/screens/CartScreen.js
@@ -1,16 +1,68 @@
-import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { CustomButton } from "../components/buttons";
 import CartItems from "../components/CartScreenComponents/CartItems";
 import CediSign from "../components/CediSign";
 import HeadTitleWithBackIcon from "../components/HeadTitleWithBackIcon";
 import  Colors  from "../data/colors";
+import { auth, firestore } from "../../BackendDirectory/config";
 
 
 function CartScreen({navigation}) {
+
+    const [ totalAmount, setTotalAmount ] = useState(null);
+    const [ deleted, setDeleted ] = useState(false);
+
+    const fetchProducts = async () => {
+        try {
+            
+            let productData = [];
+            let products = [];
+
+            await firestore.collection('cart')
+            .orderBy('postTime', 'desc')
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach(doc => {
+                    const { price, userId } = doc.data();
+                    productData.push({
+                        id: doc.id,
+                        price,
+                        userId,
+                    })
+                })
+            })
+
+            
+            productData && productData.map((product) => {
+                {auth.currentUser.uid === product.userId ? 
+                products.push(product.price)
+                : null }
+            })
+            
+            let prices = products.reduce((accumulator, currentValue) => {
+                const numericValue = parseFloat(currentValue);
+                return accumulator + numericValue;
+            }, 0);
+
+            setTotalAmount(prices);
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    useEffect(() => {
+        fetchProducts();
+        setDeleted(false)
+    }, [deleted, totalAmount]);
+
     return (
         <View style={styles.container}>
-            <ScrollView>
             <View>
                 <HeadTitleWithBackIcon previousScreen={() => navigation.navigate("Home")} title={"Cart"} />
             </View>
@@ -21,12 +73,11 @@ function CartScreen({navigation}) {
                     <Text style={styles.totalText}>Total</Text>
                 </View>
                 <View>
-                    <Text style={styles.totalAmount}><CediSign />100</Text>
+                    <Text style={styles.totalAmount}><CediSign /> {totalAmount}</Text>
                 </View>
                 </View>
                 <CustomButton onPress={() => alert("Checked all items out!")} buttonText={"Check out"}  />
             </View>
-            </ScrollView>
         </View>
     )
 }
