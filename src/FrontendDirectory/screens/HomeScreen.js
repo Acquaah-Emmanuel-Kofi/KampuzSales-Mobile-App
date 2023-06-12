650f7849174979f475982602bd96f15572ca3d12
waglarry <emmanuelacquaah2468@gmail.com>
2023-06-12 02:42:21 +0000

    Update HomeScreen with data and passing it...

--- src/FrontendDirectory/screens/HomeScreen.js
+++ src/FrontendDirectory/screens/HomeScreen.js
@@ -1,20 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, View, RefreshControl } from "react-native";
 import HomeNavbar from "../components/HomeScreenComponents/MainNavbar";
 import HomeProducts from "../components/HomeScreenComponents/HomeProducts";
 import  AppColors  from "../data/Colors";
+import { firestore } from "../../BackendDirectory/config";
 
 function HomeScreen() {
 
   const [ refresh, setRefresh ] = useState(false);
+  const [ posts, setPosts ] = useState([]);
+
+  const fetchProducts = async () => {
+    let productData = [];
+
+    await firestore.collection('products')
+    .orderBy('postTime', 'desc')
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach(doc => {
+            const { productTitle, productImages, description, category ,price, postTime, userId} = doc.data();
+            productData.push({
+                id: doc.id,
+                userPostId: userId,
+                name: productTitle,
+                image: productImages,
+                description,
+                price,
+                category,
+                postTime,
+            })
+        })
+    })
+    .catch((error) => {
+        return;
+    })
+
+    setPosts(productData);
+}
 
   const pullToRefresh = () => {
     setRefresh(true);
 
     setTimeout(() => {
       setRefresh(false);
+      fetchProducts();
     }, 2000)
   }
+
+  useEffect(() => {
+    fetchProducts();
+}, []);
 
     return (
         <View style={styles.container}>            
@@ -28,7 +63,7 @@ function HomeScreen() {
                 />
               }
               >
-              <HomeProducts />
+              <HomeProducts products={posts} />
             </ScrollView>
         </View>
     )
