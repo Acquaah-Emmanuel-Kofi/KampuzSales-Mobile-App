e7be8f9d91ce0f23d3369daa0299ef4f3552d410
waglarry <emmanuelacquaah2468@gmail.com>
2023-06-15 10:23:56 +0000

    delete PaymentDetails and WelcomeScreen

--- src/BackendDirectory/authentications/authentications.js
+++ src/BackendDirectory/authentications/authentications.js
@@ -1,14 +1,19 @@
 import { Alert } from "react-native";
 import { auth, firestore, firebase } from "../config";
 
+export let authLoading = false;
+
+
 export const loginUser = async (email, password) => {
     try {
         if( email !== "" || password !== ""){
             await auth.signInWithEmailAndPassword(email, password);
+            authLoading = true;
         } else {
             alert("Email and Password Fields are required!")
         }
     } catch (error) {
+        authLoading = false;
         if ( error.message.includes("(auth/user-not-found)")) {
             alert("There is no user record corresponding to this credentials.")
         } else if ( error.message.includes("(auth/invalid-email)")){
@@ -27,57 +32,81 @@ export const loginUser = async (email, password) => {
     }
 }
 
-export const registerUser = async (email, password, username, phoneNumber) => {
+export const registerUser = async (email, password, confirmPassword, username, phoneNumber) => {
 
-    if( email !== "" || password !== "" || username !== "" || phoneNumber !== ""){
+    if( email !== "" || password !== "" || confirmPassword !== "" || username !== "" || phoneNumber !== ""){
+        authLoading = true;
 
-        await auth.createUserWithEmailAndPassword(email, password, username, phoneNumber)
-        .then(() => {
-            auth.currentUser.sendEmailVerification({
-                handleCodeInApp: true,
-                url: 'https://kampuzsales.firebaseapp.com',
-            })
-            .then(() => {
-                Alert.alert(
-                    "Verification!",
-                    "Verification link has been sent to your email. Please verify your account!"
-                )
-            })
-            .catch(() => {
-                alert("Couldn't send email verification link!")
-            })
-            .then(() => {
-                firestore.collection('users')
-                .doc(auth.currentUser.uid)
-                .set({
-                    username: username,
-                    email: email,
-                    phoneNumber: phoneNumber,
-                    joinedDate: firebase.firestore.Timestamp.fromDate(new Date()),
-                })
-            })
-            .catch((error) => {
-                console.log(error.message);
-            })
-        })
-        .catch((error) => {
-            console.log(error);
-            if( error.message.includes("(auth/invalid-email)") ){
-                Alert.alert(
-                    "Invalid-Email",
-                    "Your email address is badly formatted. Please enter a valid email address!"
-                )
-            } else if ( error.message.includes("(auth/email-already-in-use)")){
-                Alert.alert(
-                    "Invalid-Email",
-                    "The email address is already in use by another account."
-                )
-            } else {
-                alert("All input fields are required!")
-            }
-
-        })
+        if(phoneNumber && phoneNumber.length < 10){
+            Alert.alert(
+                "Invalid Phone Number!",
+                "Please check the length of your phone number."
+            )
         } else {
+        if(password && confirmPassword && password.length < 8){
+            authLoading = false;
+            Alert.alert(
+                "Weak Password!",
+                "Password length must be more than 8 characters."
+            )
+        } else {
+            authLoading = true;
+            if(password !== confirmPassword){
+                authLoading = false;
+                alert("Password does not match!");
+            } else {
+                authLoading = true;
+                await auth.createUserWithEmailAndPassword(email, password, username, phoneNumber)
+                .then(() => {
+                    auth.currentUser.sendEmailVerification({
+                        handleCodeInApp: true,
+                        url: 'https://kampuzsales-3cb39.firebaseapp.com',
+                    })
+                    .then(() => {
+                        Alert.alert(
+                            "Verification!",
+                            "Verification link has been sent to your email. Please verify your account!"
+                        )
+                    })
+                    .catch(() => {
+                        alert("Couldn't send email verification link!")
+                    })
+                    .then(() => {
+                        firestore.collection('users')
+                        .doc(auth.currentUser.uid)
+                        .set({
+                            username: username,
+                            email: email,
+                            phoneNumber: phoneNumber,
+                            firstPost: true,
+                            joinedDate: firebase.firestore.Timestamp.fromDate(new Date()),
+                        })
+                    })
+                    .catch((error) => {
+                        alert(error.message);
+                    })
+                })
+                .catch((error) => {
+                    authLoading = false;
+                    if( email && error.message.includes("(auth/invalid-email)") ){
+                        Alert.alert(
+                            "Invalid-Email",
+                            "Your email address is badly formatted. Please enter a valid email address!"
+                        )
+                    } else if ( email && error.message.includes("(auth/email-already-in-use)")){
+                        Alert.alert(
+                            "Invalid-Email",
+                            "The email address is already in use by another account."
+                        )
+                    } else {
+                        alert("All input fields are required!")
+                    }
+                })
+                }
+            }
+        }
+        } else {
+            authLoading = false;
             alert("All input fields are required!")
         }
 }
@@ -121,3 +150,26 @@ export const sendPasswordResetLink = (email) => {
         }
     })
 } 
+
+//change passwaord
+
+// const user = firebase.auth().currentUser;
+// const newPassword = getASecureRandomPassword();
+
+// user.updatePassword(newPassword).then(() => {
+//   // Update successful.
+// }).catch((error) => {
+//   // An error ocurred
+//   // ...
+// });
+
+// delete account
+
+// const user = firebase.auth().currentUser;
+
+// user.delete().then(() => {
+//   // User deleted.
+// }).catch((error) => {
+//   // An error ocurred
+//   // ...
+// });
