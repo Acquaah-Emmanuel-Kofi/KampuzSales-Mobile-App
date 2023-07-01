9040580f32b0939011dee40b47a59698def3cf7d
waglarry <emmanuelacquaah2468@gmail.com>
2023-07-01 14:10:48 +0000

    update auth component with more functions

--- src/BackendDirectory/authentications/authentications.js
+++ src/BackendDirectory/authentications/authentications.js
@@ -5,17 +5,22 @@ export let authLoading = false;
 
 
 export const loginUser = async (email, password) => {
+    authLoading = true;
     try {
         if( email !== "" || password !== ""){
             await auth.signInWithEmailAndPassword(email, password);
             authLoading = true;
         } else {
+            authLoading = false;
             alert("Email and Password Fields are required!")
         }
     } catch (error) {
         authLoading = false;
         if ( error.message.includes("(auth/user-not-found)")) {
-            alert("There is no user record corresponding to this credentials.")
+            Alert.alert(
+                "Invalid Credentials!",
+                "There is no user record corresponding to this credentials."
+            )
         } else if ( error.message.includes("(auth/invalid-email)")){
             Alert.alert(
                 "Invalid-Email",
@@ -32,8 +37,10 @@ export const loginUser = async (email, password) => {
     }
 }
 
-export const registerUser = async (email, password, confirmPassword, username, phoneNumber) => {
 
+export const registerUser = async (email, password, confirmPassword, username, phoneNumber) => {
+    let validPassword = checkPasswordHealth(password);
+    
     if( email !== "" || password !== "" || confirmPassword !== "" || username !== "" || phoneNumber !== ""){
         authLoading = true;
 
@@ -43,71 +50,67 @@ export const registerUser = async (email, password, confirmPassword, username, p
                 "Please check the length of your phone number."
             )
         } else {
-        if(password && confirmPassword && password.length < 8){
-            authLoading = false;
-            Alert.alert(
-                "Weak Password!",
-                "Password length must be more than 8 characters."
-            )
-        } else {
-            authLoading = true;
-            if(password !== confirmPassword){
-                authLoading = false;
-                alert("Password does not match!");
+            if(!validPassword.isValid){
+                Alert.alert(
+                    "Invalid Password!",
+                    validPassword.errors
+                )
             } else {
                 authLoading = true;
-                await auth.createUserWithEmailAndPassword(email, password, username, phoneNumber)
-                .then(() => {
-                    auth.currentUser.sendEmailVerification({
-                        handleCodeInApp: true,
-                        url: 'https://kampuzsales-3cb39.firebaseapp.com',
-                    })
+                if(password !== confirmPassword){
+                    authLoading = false;
+                    alert("Password does not match!");
+                } else {
+                    authLoading = true;
+                    await auth.createUserWithEmailAndPassword(email, password, username, phoneNumber)
                     .then(() => {
-                        Alert.alert(
-                            "Verification!",
-                            "Verification link has been sent to your email. Please verify your account!"
-                        )
-                    })
-                    .catch(() => {
-                        alert("Couldn't send email verification link!")
-                    })
-                    .then(() => {
-                        firestore.collection('users')
-                        .doc(auth.currentUser.uid)
-                        .set({
-                            username: username,
-                            email: email,
-                            phoneNumber: phoneNumber,
-                            firstPost: true,
-                            joinedDate: firebase.firestore.Timestamp.fromDate(new Date()),
+                        auth.currentUser.sendEmailVerification({
+                            handleCodeInApp: true,
+                            url: 'https://kampuzsales-3cb39.firebaseapp.com',
+                        })
+                        .then(() => {
+                            Alert.alert(
+                                "Verification!",
+                                "Verification link has been sent to your email. Please verify your account!"
+                            )
+                        })
+                        .catch(() => {
+                            alert("Couldn't send email verification link!")
+                        })
+                        .then(() => {
+                            firestore.collection('users')
+                            .doc(auth.currentUser.uid)
+                            .set({
+                                username: username,
+                                email: email,
+                                phoneNumber: phoneNumber,
+                                firstPost: true,
+                                joinedDate: firebase.firestore.Timestamp.fromDate(new Date()),
+                            })
+                        })
+                        .catch((error) => {
+                            alert(error.message);
                         })
                     })
                     .catch((error) => {
-                        alert(error.message);
+                        authLoading = false;
+                        if( email && error.message.includes("(auth/invalid-email)") ){
+                            Alert.alert(
+                                "Invalid-Email",
+                                "Your email address is badly formatted. Please enter a valid email address!"
+                            )
+                        } else if ( email && error.message.includes("(auth/email-already-in-use)")){
+                            Alert.alert(
+                                "Invalid-Email",
+                                "The email address is already in use by another account."
+                            )
+                        } else {
+                            alert("All input fields are required!")
+                        }
                     })
-                })
-                .catch((error) => {
-                    authLoading = false;
-                    if( email && error.message.includes("(auth/invalid-email)") ){
-                        Alert.alert(
-                            "Invalid-Email",
-                            "Your email address is badly formatted. Please enter a valid email address!"
-                        )
-                    } else if ( email && error.message.includes("(auth/email-already-in-use)")){
-                        Alert.alert(
-                            "Invalid-Email",
-                            "The email address is already in use by another account."
-                        )
-                    } else {
-                        alert("All input fields are required!")
                     }
-                })
-                }
             }
-        }
-        } else {
-            authLoading = false;
-            alert("All input fields are required!")
+            }
         }
 }
 
@@ -151,6 +154,44 @@ export const sendPasswordResetLink = (email) => {
     })
 } 
 
+export const handleLogout = () => {
+    Alert.alert(
+      'Logging Out',
+      'Are you sure you want to logout?',
+      [
+          {
+              text: 'Cancel',
+              onPress: () => {},
+              style: 'cancel'
+          },
+          {
+              text: 'Logout',
+              onPress: () => auth.signOut(),
+          }
+      ],
+      { cancelable: false}
+    )
+  }
+
+  export const handleDeleteAccount = () => {
+    Alert.alert(
+      'Delete Account Forever',
+      'Are you sure you want to delete this account?',
+      [
+          {
+              text: 'Cancel',
+              onPress: () => {},
+            //   style: 'cancel'
+          },
+          {
+              text: 'Delete',
+              onPress: () => deleteAccount(),
+          }
+      ],
+      { cancelable: false}
+    )
+  }
+
 //change passwaord
 
 // const user = firebase.auth().currentUser;
@@ -163,13 +204,101 @@ export const sendPasswordResetLink = (email) => {
 //   // ...
 // });
 
-// delete account
 
-// const user = firebase.auth().currentUser;
+const deleteAccount = () => {
+    const user = auth.currentUser;
 
-// user.delete().then(() => {
-//   // User deleted.
-// }).catch((error) => {
-//   // An error ocurred
-//   // ...
-// });
+        let userId;
+
+        firestore.collection('users')
+        .doc(auth.currentUser.uid)
+        .get()
+        .then((snapshot) => {
+            if(snapshot.exists){
+              userId = snapshot.id; 
+            }
+        })
+        .catch((error) => {
+          alert(error.message);
+        })
+
+    firestore.collection('users')
+    .doc(userId)
+    .delete()
+    .then(() => {
+        user.delete().then(() => {
+            // User deleted.
+                Alert.alert(
+                    'Account Deleted Successfully!',
+                    "😪We're sorry if we couldn't serve you well. Love to see you here back again."
+                )
+            }).catch((error) => {
+            // An error ocurred
+            alert(error.message)
+            });
+    })
+    .catch((error) => {
+        console.log(error.message);
+    })
+
+    // const credential = promptForCredentials();
+
+    // reauthenticateWithCredential(user, credential).then(() => {
+    // // User re-authenticated.
+    // }).catch((error) => {
+    // // An error ocurred
+    // // ...
+    // });
+}
+
+function checkPasswordHealth(password) {
+    const minLength = 8;
+    const minUpperCase = 1;
+    const minLowerCase = 1;
+    const minNumbers = 1;
+    const minSpecialChars = 1;
+  
+    let score = 0;
+    const errors = [];
+  
+    // Check minimum length
+    if (password.length < minLength) {
+      errors.push(`Password should be at least ${minLength} characters long.`);
+    } else {
+      score++;
+    }
+  
+    // Check uppercase letters
+    if (/[A-Z]/.test(password)) {
+      score++;
+    } else {
+      errors.push(`Password should contain at least ${minUpperCase} uppercase letter.`);
+    }
+  
+    // Check lowercase letters
+    if (/[a-z]/.test(password)) {
+      score++;
+    } else {
+      errors.push(`Password should contain at least ${minLowerCase} lowercase letter.`);
+    }
+  
+    // Check numbers
+    if (/[0-9]/.test(password)) {
+      score++;
+    } else {
+      errors.push(`Password should contain at least ${minNumbers} number.`);
+    }
+  
+    // Check special characters
+    if (/[^A-Za-z0-9]/.test(password)) {
+      score++;
+    } else {
+      errors.push(`Password should contain at least ${minSpecialChars} special character.`);
+    }
+  
+    return {
+      score: score,
+      isValid: score === 5,
+      errors: errors
+    };
+  }
