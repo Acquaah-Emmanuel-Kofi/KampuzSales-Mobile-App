7773bfc49dc31a2bb517f322d93b7814a6d5a79a
waglarry <emmanuelacquaah2468@gmail.com>
2023-07-20 01:41:50 +0000

    Update functions.js
    Add increasePostCount function and add new comments

--- src/BackendDirectory/functionalities/functions.js
+++ src/BackendDirectory/functionalities/functions.js
@@ -1,35 +1,55 @@
 import { Alert } from "react-native";
-import { storage } from "../config";
+import { auth, firestore, storage } from "../config";
 import urlToBlob from "./urlToBlob";
+// import { useNavigation } from "@react-navigation/native";
 
 
 export const isValidDigitalAddress = (address) => {
-// Regular expression for validating a custom digital address
-const addressRegex = /^[A-Za-z0-9\-_]+$/;
-return addressRegex.test(address);
+    // Regular expression for validating a custom digital address
+    const addressRegex = /^[A-Za-z0-9\-_]+$/;
+    
+    return addressRegex.test(address);
 };
 
     // Function to generate a new download link for an image in a different folder
 export const generateNewDownloadLink = async (imageURL, newFolder) => {
-try {
-    // Convert the image URL to Blob
-    const blob = await urlToBlob(imageURL);
+    try {
+        // Convert the image URL to Blob
+        const blob = await urlToBlob(imageURL);
 
-    // Create a reference to the new image in the new folder
-    const newImageRef = storage.ref().child(`${newFolder}/${new Date().getTime()}.jpg`);
+        // Create a reference to the new image in the new folder
+        const newImageRef = storage.ref().child(`${newFolder}/${new Date().getTime()}.jpg`);
 
-    // Upload the blobbed image to the new folder
-    const snapshot = await newImageRef.put(blob);
+        // Upload the blobbed image to the new folder
+        const snapshot = await newImageRef.put(blob);
 
-    // Get the new download URL for the image in the new folder
-    const newDownloadURL = await snapshot.ref.getDownloadURL();
+        // Get the new download URL for the image in the new folder
+        const newDownloadURL = await snapshot.ref.getDownloadURL();
 
-    return newDownloadURL;
+        return newDownloadURL;
 
-} catch (error) {
-    Alert.alert(
-        'Failed Adding Product!',
-        error.message
-    )
+    } catch (error) {
+        Alert.alert(
+            'Failed Adding Product!',
+            error.message
+        )
+    }
 }
-}
+
+export const increasePostCount = async () => {
+    const sellerRef = firestore.collection('sellers');
+    const sellerSnapshot = await sellerRef.get();
+  
+    sellerSnapshot.forEach((doc) => {
+      const data = doc.data();
+  
+      if (auth.currentUser.uid === data.userId) {
+        const updatedPostCount = data.numOfProductsPosted + 1;
+  
+        sellerRef.doc(doc.id)
+          .update({
+            numOfProductsPosted: updatedPostCount,
+          })
+      }
+    });
+  };
