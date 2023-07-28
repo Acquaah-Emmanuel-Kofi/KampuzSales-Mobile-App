3583a8e0e627e689cc10c5116257bed96922ea72
waglarry <emmanuelacquaah2468@gmail.com>
2023-07-28 01:31:58 +0000

    Create Alert and AccountSettings screen

---
+++ src/FrontendDirectory/components/NotificationComponent/AlertScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+const AlertScreen = ({notifications}) => {
+
+    console.log(notifications);
+    
+    return (
+        <View>
+            <Text>Alert</Text>
+        </View>
+    );
+}
+
+export default AlertScreen;
---
+++ src/FrontendDirectory/screens/AccountSettingsScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AccountSettingsScreen = () => {
+    return (
+        <View>
+            <Text>Account Settings</Text>
+        </View>
+    );
+}
+
+export default AccountSettingsScreen;
