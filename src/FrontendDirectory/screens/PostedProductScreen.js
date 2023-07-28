84a0e1b3bb800d41a219758fb12953650d24fb4a
waglarry <emmanuelacquaah2468@gmail.com>
2023-07-28 01:50:01 +0000

    Update PostedProductScreen.js

--- src/FrontendDirectory/screens/PostedProductScreen.js
+++ src/FrontendDirectory/screens/PostedProductScreen.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 import HeadTitle from "../components/HeadTitle";
 import AppColors from "../data/Colors";
-import PostedProducts from "../components/PostedProductsScreenComponents/PostedProducts";
+import PostedProducts from "../components/PostedProductsComponents/PostedProducts";
 
 function PostedProductScreen() {
     return (
         <SafeAreaView style={styles.container}>
             <HeadTitle title={"Products Posted"} />
+            <View style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: AppColors.danger,
+              width: '90%',
+              marginHorizontal: '5%',
+              paddingHorizontal: 10,
+              paddingVertical: 10,
+              borderRadius: 8,
+            }}>
+              <Text style={{color: AppColors.white}}>Check all products that are still available at your end.</Text>
+            </View>
             <PostedProducts />
         </SafeAreaView>
     )
