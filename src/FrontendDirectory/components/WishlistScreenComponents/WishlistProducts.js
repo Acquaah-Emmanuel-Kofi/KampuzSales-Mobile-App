8ccb699a7c6ea1e66e94b51454e2035b94da6a71
waglarry <emmanuelacquaah2468@gmail.com>
2023-08-01 10:12:15 +0000

    Update WishlistProducts.js

--- src/FrontendDirectory/components/WishlistScreenComponents/WishlistProducts.js
+++ src/FrontendDirectory/components/WishlistScreenComponents/WishlistProducts.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Alert, Image, Pressable, RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Alert, Image, Pressable, RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import AppColors from "../../data/Colors";
-import { auth, firestore, storage } from "../../../BackendDirectory/config";
+import { auth, firestore, storage, firebase } from "../../../BackendDirectory/config";
 import Fontisto from "react-native-vector-icons/Fontisto";
 import CediSign from "../CediSign";
+import { generateNewDownloadLink } from "../../../BackendDirectory/functionalities/functions";
+import { CustomButton } from "../buttons";
+
 
 function WishlistProducts() {
     const navigation = useNavigation();
     
     const [ dataFromState, setDataFromState ] = useState([]);
+    const [ product, setProduct ] = useState(null);
+    const [ cartIds, setCartIds ] = useState([]);
     const [ deleted, setDeleted ] = useState(false);
     const [ refresh, setRefresh ] = useState(false);
-
+    const [ uploading, setUploading ] = useState(false);
 
     const fetchProducts = async () => {
         try {
@@ -24,24 +29,74 @@ function WishlistProducts() {
             .get()
             .then((querySnapshot) => {
                 querySnapshot.forEach(doc => {
-                    const { productTitle, productImage, description, price, postTime, userId} = doc.data();
+                    const { productTitle, productImage, description, price, postTime, userId } = doc.data();
                     productData.push({
                         id: doc.id,
-                        name: productTitle,
+                        productTitle,
                         image: productImage,
                         description,
                         price,
                         postTime,
                         userId
                     })
+
+                    let data = {
+                        details: doc.data(),
+                        productId : doc.id
+                    }
+                    setProduct(data)
                 })
             })
 
             setDataFromState(productData);
 
         } catch (error) {
-            console.log(error.message);
+            return;
         }
+    }
+
+    const addToCart = async (originalUrl, newFolder, productId) => {
+        if(cartIds && cartIds.includes(product.id)){
+            alert("Item is already in cart!");
+            setUploading(false);
+        } else {
+            setUploading(true);
+            let imageUrl = await generateNewDownloadLink(originalUrl, newFolder);
+            
+            if(imageUrl){
+                cartDetails(imageUrl, productId);
+                setUploading(false)
+
+                navigation.navigate("Cart");
+            }
+        }
+    }
+
+    const cartDetails = (imageUrl, productId) => {
+        firestore.collection('cart')
+        .add({
+            userId: auth.currentUser.uid,
+            productTitle: product?.details?.productTitle,
+            productId: product?.productId,
+            price: product?.details?.price,
+            category: product?.details?.category,
+            productImage: imageUrl ? imageUrl : product?.details?.productImage,
+            description: product?.details?.description,
+            postTime: firebase.firestore.Timestamp.fromDate(new Date()),
+        })
+        .then(() => {
+            Alert.alert(
+                'Added Cart!',
+                'Your product is added successfully!',
+            );
+            deletePost(productId)
+            setUploading(false)
+        })
+        .catch((error) => {
+            setUploading(false)
+            alert(error.message)
+            console.log(error);
+        })
     }
 
     const handleDeleteModal = (postId) => {
@@ -51,7 +106,6 @@ function WishlistProducts() {
             [
                 {
                     text: 'Cancel',
-                    onPress: () => console.log(""),
                     style: 'cancel'
                 },
                 {
@@ -110,9 +164,29 @@ function WishlistProducts() {
         })
     }
 
+    const getCartIds = async () => {
+        
+        let productIds = [];
+        await firestore.collection("cart")
+        .orderBy('postTime', 'desc')
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach(doc => {
+                const { userId, productId } = doc.data();
+
+                {auth.currentUser.uid === userId ? 
+                    (productIds.push(productId))
+                : null }
+                
+            })
+        })
+        setCartIds(productIds);
+    }
+
     useEffect(() => {
         fetchProducts();
-    }, [deleted]);
+        getCartIds();
+    }, []);
 
     useEffect(() => {
         fetchProducts();
@@ -146,11 +220,17 @@ function WishlistProducts() {
                     {auth.currentUser.uid === product.userId ? 
                         <Pressable style={styles.productBox} onPress={() => navigation.navigate("Single", product)}>
                             <View style={styles.imageBox}>
-                                <Image style={styles.image} source={{uri: product.image}} alt={product.name} />
+                                <Image style={styles.image} source={{uri: product.image}} alt={product.productTitle} />
                             </View>
                             <View style={styles.productDetailsBox}>
-                                <Text style={styles.productName}>{product.name}</Text>
+                                <Text style={styles.productName}>{product.productTitle}</Text>
                                 <Text style={styles.productPrice}><CediSign /> {product.price}</Text>
+                            <Pressable 
+                                style={styles.addToCart}
+                                disabled={uploading ? true : false} 
+                                onPress={() => addToCart(product?.image, "cart", product.id)}>
+                                <Text style={{color: AppColors.white}}>{ uploading ? <ActivityIndicator size="small" color={AppColors.white} /> : "Add to Cart"}</Text>
+                            </Pressable>
                             </View>
                             <Pressable onPress={() => handleDeleteModal(product.id)}>
                                 <Fontisto name="close" size={24} color={AppColors.black} />
@@ -161,6 +241,11 @@ function WishlistProducts() {
                 ))
             }
             </ScrollView>
+                <View style={styles.totalMarginHorizontal}>
+                    <CustomButton onPress={() => alert("Checked all items out!")} buttonText={"Add all to my cart"}  />
+                </View>
+            {/* { dataFromState.length > 1 ? (
+            ) : null} */}
         </View>
     )
 }
@@ -186,7 +271,14 @@ const styles = StyleSheet.create({
         borderRadius: 10,
     },
     productDetailsBox: {
-        width: '55%'
+        ...Platform.select({
+            ios: {
+                width: '55%',
+            },
+            android: {
+                width: '45%',
+            },
+        })
     },
     productName: {
         color: AppColors.deepGray,
@@ -198,6 +290,18 @@ const styles = StyleSheet.create({
         color: AppColors.black,
         fontSize: 16,
         fontWeight: '600',
+    },
+    addToCart: {
+        backgroundColor: AppColors.primary,
+        width: 120,
+        height: 25,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15,
+    },
+    totalMarginHorizontal: {
+        marginBottom: 10
     },
 });
 
