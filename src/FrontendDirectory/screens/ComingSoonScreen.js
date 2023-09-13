60f45d51b52c8a49aa6cb0fc411c0f9f76f0c217
waglarry <emmanuelacquaah2468@gmail.com>
2023-09-13 23:50:33 +0000

    Create ComingSoonScreen.js

---
+++ src/FrontendDirectory/screens/ComingSoonScreen.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { StyleSheet ,Text, View , ScrollView, Image } from "react-native";
+import  AppColors  from "../data/Colors";
+import HeadTitleWithBackIcon from "../components/HeadTitleWithBackIcon";
+
+function ComingSoonScreen({navigation}) {
+    return (
+        <View style={styles.container}>
+            <HeadTitleWithBackIcon previousScreen={() => navigation.goBack()} title={"Construction"} />
+            <ScrollView 
+                contentContainerStyle={styles.content}
+                automaticallyAdjustKeyboardInsets={true}
+                alwaysBounceVertical={true}
+                automaticallyAdjustsScrollIndicatorInsets={true}
+                showsVerticalScrollIndicator={false}>
+                    {/* <Text style={styles.heading}>Coming Soon...</Text> */}
+                    <View style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                    <Image style={{width: 250, height: 250}} source={require("../../../assets/marketing.png")} />
+                    <Text style={{
+                        marginTop: 20,
+                        fontSize: 18,
+                    }}>Coming Soon...</Text>
+                    <Text style={{textAlign: 'center', marginVertical: 10}}>
+                        This screen is still under construction. 
+                        
+                    </Text>
+                    <Text style={{fontSize: 12, color: AppColors.primary}}>Happy shopping!</Text>
+                </View>
+                </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: AppColors.white,
+      justifyContent: 'center',
+    },
+    content: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: '30%',
+    },
+    heading: {
+        fontSize: 30,
+        fontWeight: '600',
+        color: AppColors.black,
+        marginBottom: 20,
+        letterSpacing: 2,
+    }
+  });
+
+export default ComingSoonScreen;
