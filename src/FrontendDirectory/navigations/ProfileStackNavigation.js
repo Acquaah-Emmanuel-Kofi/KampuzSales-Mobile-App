18f7f6a4f08824c02cd98b94821353a2fddcaad5
waglarry <emmanuelacquaah2468@gmail.com>
2023-09-14 17:33:25 +0000

    Update ProfileStackNavigation.js
    Add new components

--- src/FrontendDirectory/navigations/ProfileStackNavigation.js
+++ src/FrontendDirectory/navigations/ProfileStackNavigation.js
@@ -12,6 +12,9 @@ import UpdateEmailScreen from '../screens/UpdateEmailScreen';
 import UpdatePasswordScreen from '../screens/UpdatePasswordScreen';
 import ComingSoonScreen from '../screens/ComingSoonScreen';
 import RequestAcceptanceScreen from '../screens/RequestAcceptanceScreen';
+import SalesHistoryScreen from '../screens/SalesHistoryScreen';
+import OrderHistoryScreen from '../screens/OrderHistoryScreen';
+import FAQScreen from '../screens/FAQScreen';
 
 const Tab = createNativeStackNavigator();
 
@@ -34,7 +37,10 @@ const ProfileStackNavigation = () => {
             <Tab.Screen name='Single' component={SingleProductDetailsScreen}></Tab.Screen>
             <Tab.Screen name='EditProduct' component={EditProductDetails}></Tab.Screen>
             <Tab.Screen name='EditProfile' component={EditProfileScreen}></Tab.Screen>
+            <Tab.Screen name='SalesHistory' component={SalesHistoryScreen}></Tab.Screen>
+            <Tab.Screen name='OrderHistory' component={OrderHistoryScreen}></Tab.Screen>
             <Tab.Screen name='AccountSettings' component={AccountSettingsScreen}></Tab.Screen>
+            <Tab.Screen name='FAQ' component={FAQScreen}></Tab.Screen>
             <Tab.Screen name='AboutKampuzSales' component={AboutKampuzSales}></Tab.Screen>
             <Tab.Screen name='UpdateEmail' component={UpdateEmailScreen}></Tab.Screen>
             <Tab.Screen name='UpdatePassword' component={UpdatePasswordScreen}></Tab.Screen>
